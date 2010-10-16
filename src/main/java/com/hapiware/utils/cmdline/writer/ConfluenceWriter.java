fdab335c1d83bb7e1bddd0eb7e5975d20cb575dc
Hapi <hapi@hapiware.com>
2010-10-16 03:45:46 +0300

    Added writers for Wikidot and Confluence.

---
+++ src/main/java/com/hapiware/utils/cmdline/writer/ConfluenceWriter.java
@@ -0,0 +1,111 @@
+package com.hapiware.utils.cmdline.writer;
+
+import java.io.PrintStream;
+
+
+/**
+ * Headings and list items are one liners only (i.e. if they are longer than defined screen
+ * width they are just cut).
+ * 
+ * @author <a href="http://www.hapiware.com" target="_blank">hapi</a>
+ *
+ */
+public class ConfluenceWriter
+	implements
+		Writer
+{
+	private final static PrintStream STREAM = System.out;
+	
+	
+	public ConfluenceWriter()
+	{
+		// Does nothing.
+	}
+
+	public void h1(String text)
+	{
+		STREAM.println("h1. " + text);
+	}
+
+	public void h2(String text)
+	{
+		STREAM.println("h2. " + text);
+	}
+
+	public void h3(String text)
+	{
+		STREAM.println("h3. " + text);
+	}
+
+	public void h4(String text)
+	{
+		STREAM.println("h4. " + text);
+	}
+
+	public void h5(String text)
+	{
+		STREAM.println("h5. " + text);
+	}
+
+	public void line(HeadingLevel headingLevel, String text)
+	{
+		STREAM.println(text + " \\\\");
+	}
+
+	public void paragraph(HeadingLevel headingLevel, String text)
+	{
+		STREAM.println(text);
+		STREAM.println();
+	}
+
+	public String strongBegin()
+	{
+		return "*";
+	}
+
+	public String strongEnd()
+	{
+		return "*";
+	}
+
+	public void listBegin(HeadingLevel headingLevel)
+	{
+		// Does nothing.
+	}
+
+	public void listItem(String text)
+	{
+		STREAM.println("* " + text);
+	}
+
+	public void listEnd()
+	{
+		STREAM.println();
+	}
+
+	public void codeBegin(HeadingLevel headingLevel)
+	{
+		STREAM.println("{code}");
+	}
+	
+	public void codeLine(String code)
+	{
+		STREAM.println(code);
+	}
+	
+	public void codeEnd()
+	{
+		STREAM.println("{code}");
+		STREAM.println();
+	}
+	
+	public void footer()
+	{
+		// Does nothing.
+	}
+	
+	public void header()
+	{
+		// Does nothing.
+	}
+}
--- src/main/java/com/hapiware/utils/cmdline/writer/ScreenWriter.java
+++ src/main/java/com/hapiware/utils/cmdline/writer/ScreenWriter.java
@@ -1,5 +1,7 @@
 package com.hapiware.utils.cmdline.writer;
 
+import java.io.PrintStream;
+
 import com.hapiware.utils.cmdline.Util;
 
 
@@ -17,13 +19,15 @@ public class ScreenWriter
 	private final static int TAB_SIZE = 4;
 	private final static int MIN_SCREEN_WIDTH = 40;
 	private final static int MAX_SCREEN_WIDTH = 250;
+	private final static PrintStream STREAM = System.out;
+	
 	private final int _screenWidth;
 	private HeadingLevel _headingLevelForListItems;
 	private HeadingLevel _headingLevelForCodeLines;
 	
 	public ScreenWriter(int screenWidth)
 	{
-		if(screenWidth < 40)
+		if(screenWidth < MIN_SCREEN_WIDTH || screenWidth > MAX_SCREEN_WIDTH)
 			throw
 				new IllegalArgumentException(
 					screenWidth + " is not an appropriate value for 'screenWidth'. "
@@ -34,38 +38,38 @@ public class ScreenWriter
 
 	public void h1(String text)
 	{
-		System.out.println(cut(text));
+		STREAM.println(cut(text));
 	}
 
 	public void h2(String text)
 	{
-		System.out.println(cut(tab(HeadingLevel.H1) + text));
+		STREAM.println(cut(tab(HeadingLevel.H1) + text));
 	}
 
 	public void h3(String text)
 	{
-		System.out.println(cut(tab(HeadingLevel.H2) + text));
+		STREAM.println(cut(tab(HeadingLevel.H2) + text));
 	}
 
 	public void h4(String text)
 	{
-		System.out.println(cut(tab(HeadingLevel.H3) + text));
+		STREAM.println(cut(tab(HeadingLevel.H3) + text));
 	}
 
 	public void h5(String text)
 	{
-		System.out.println(cut(tab(HeadingLevel.H4) + text));
+		STREAM.println(cut(tab(HeadingLevel.H4) + text));
 	}
 
 	public void line(HeadingLevel headingLevel, String text)
 	{
-		System.out.println(cut(tab(headingLevel) + text));
+		STREAM.println(cut(tab(headingLevel) + text));
 	}
 
 	public void paragraph(HeadingLevel headingLevel, String text)
 	{
-		Util.write(text, TAB_SIZE * (headingLevel.ordinal() + 1), _screenWidth, System.out);
-		System.out.println();
+		Util.write(text, TAB_SIZE * (headingLevel.ordinal() + 1), _screenWidth, STREAM);
+		STREAM.println();
 	}
 
 	public String strongBegin()
@@ -85,12 +89,12 @@ public class ScreenWriter
 
 	public void listItem(String text)
 	{
-		System.out.println(cut(tab(_headingLevelForListItems) + "* " + text));
+		STREAM.println(cut(tab(_headingLevelForListItems) + "* " + text));
 	}
 
 	public void listEnd()
 	{
-		System.out.println();
+		STREAM.println();
 	}
 
 	public void codeBegin(HeadingLevel headingLevel)
@@ -105,7 +109,7 @@ public class ScreenWriter
 	
 	public void codeEnd()
 	{
-		System.out.println();
+		STREAM.println();
 	}
 	
 	public void footer()
